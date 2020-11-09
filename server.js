bf7cf9b4219d554cb94cdea408d148ad1bb3df97
AustenHsiao <Austenhsiao@gmail.com>
2020-11-09 13:37:02 -0800

    test

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const http = require('http');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-    
+    let url = req.url;
+    if(url == '/'){
+        res.write("HI");
+    }
     res.end();
 };
 
