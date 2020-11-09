a732f7a428f41498cf11366fdfd6483ea74a4d61
AustenHsiao <Austenhsiao@gmail.com>
2020-11-09 13:57:42 -0800

    testing css

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const port = process.env.PORT || 5000;
 
 
-const mainPage = fs.readFileSync('index.html');
+const mainPage = fs.readFileSync('pages/index.html');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
