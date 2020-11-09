f5923ed08d04bacbb8620126fd5fec764d94a36c
AustenHsiao <Austenhsiao@gmail.com>
2020-11-09 13:49:05 -0800

    serve html

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require('http'); 
+const fs = require('fs');
 const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-    let url = req.url;
-    if(url == '/'){
-        res.write("HI");
-    }
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('index.html', (err, data) => {
+        if(err){
+            return console.log(err);
+        }
+    })
+    
     res.end();
 });
 
