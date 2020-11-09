20e9b79c03cd5c6f45940f33d156bcd056e5a08b
AustenHsiao <Austenhsiao@gmail.com>
2020-11-09 14:04:01 -0800

    serve html + css

--- server.js
+++ server.js
@@ -3,11 +3,22 @@ const fs = require('fs');
 const port = process.env.PORT || 5000;
 
 
-const mainPage = fs.readFileSync('pages/index.html');
+
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(mainPage);
+    const mainPage = fs.readFileSync('pages/index.html');
+    const cssFile = fs.readFileSync('css/forage.css');
+
+    switch (req.url) {
+        case "/css/forage.css" :
+            res.writeHead(200, {"Content-Type": "text/css"});
+            res.write(cssFile);
+            break;
+        default :    
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write(mainPage);
+    };
+    res.end();
 });
 
 server.listen(port, () => {
