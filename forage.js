7ad3e281241be97f64983c0f87ae53c4f00ca63a
AustenHsiao <Austenhsiao@gmail.com>
2020-11-15 01:17:45 -0800

    can see coordinates in left box now

--- forage.js
+++
@@ -1,7 +0,0 @@
-const initMap = () => {
-    const map = new google.maps.Map(document.getElementById("mapBox"), {
-        zoom: 13,
-        center: { lat: 45.5111, lng: -122.6834 }, //portland state university
-    });
-    document.getElementById("mapBox").appendChild(map);
-}
