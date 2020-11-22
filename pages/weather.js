4cdf3f0a829a54413a6002a482d4d9f6449d829e
sandyaspen <sandyaspenwood@gmail.com>
2020-11-22 01:40:13 -0800

    got weather api to function and display on button click

--- pages/weather.js
+++ pages/weather.js
@@ -1,42 +1,70 @@
 //HARDCODED 11/15+++++++++++++++WIP++++++++++++++++++
-const cityName = 'Portland';
-const stateCode = 'Oregon';
+let cityName = "Portland";
+let stateCode = "Oregon";
+let urlStart = "https://api.openweathermap.org/data/2.5/";
+let weatherAPIkey = "6e093cb352d4d124394962457cd432bc";
 
-const getWeather = async () => { 
-    const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=6e093cb352d4d124394962457cd432bc`);
-    if(api.status === 404){
-        console.log("Data not found");
-        return null;
-    }
+const getWeather = async () => {
+  const api = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&units=imperial&appid=6e093cb352d4d124394962457cd432bc`
+  );
+  if (api.status === 404) {
+    console.log("Data not found");
+    return null;
+  }
+  const data = await api.json();
+  return data;
+};
 
-    const data = await api.json();
-    console.log(data)
-    return data;
-}
-
-const getMap = async () => { 
-    const api = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName},${stateCode}&appid=6e093cb352d4d124394962457cd432bc`);
-    if(api.status === 404){
-        console.log("Data not found");
-        return null;
-    }
-
-    const data = await api.json();
-    console.log(data)
-    return data;
-}
-
-document.getElementById("testButton").addEventListener("click", () =>{
-    const weather = getWeather();
-    if(!weather){
-        ReactDOM.render(
-            React.createElement('h1', null, "No data found for location"),
-            document.getElementById('weatherBox')
-        );
-    }else{
-        ReactDOM.render(
-            React.createElement('h1', null, `Weather for ${cityName},${stateCode}`),
-            document.getElementById('weatherBox')
-        );
-    }
+document.getElementById("testButton").addEventListener("click", () => {
+  populateWeather();
 });
+
+async function populateWeather() {
+  let weather = await getWeather(); //fetch weather data from the API
+  let weatherBox = document.getElementById("weatherBox");
+  let weatherBoxContent;
+
+  if (!weather) {
+    //getWeather returned null, 404 error on API
+    weatherBoxContent = document.createElement("p");
+    let error_message = document.createTextNode(
+      "Unable to retrieve weather for this location."
+    );
+    weatherBoxContent.appendChild(error_message);
+    weatherBox.appendChild(weatherBoxContent);
+  } else {
+    //weather successfully retreived
+    let tbody = document.createElement("tbody");
+    let tableRow;
+    let displayValues = {
+      Temperature: weather.main.temp,
+      Humidity: weather.main.humidity,
+      Conditions: weather.weather[0].main,
+    };
+    weatherBoxContent = document.createElement("table");
+    let keys = Object.keys(displayValues);
+
+    keys.forEach((key) => {
+      tableRow = makeWeatherRow(key, displayValues[key]);
+      tbody.appendChild(tableRow);
+      weatherBoxContent.appendChild(tbody);
+    });
+
+    weatherBox.appendChild(weatherBoxContent);
+  }
+  return;
+}
+
+function makeWeatherRow(title, value) {
+  let row = document.createElement("tr");
+  let titleCell = document.createElement("td");
+  titleCell.appendChild(document.createTextNode(title));
+  let valueCell = document.createElement("td");
+  titleCell.appendChild(document.createTextNode(value));
+
+  row.appendChild(titleCell);
+  row.appendChild(valueCell);
+
+  return row;
+}
