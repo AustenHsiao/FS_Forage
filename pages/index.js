c3d9b16475888393e2e430f098b69b0a3d7fb73a
AustenHsiao <Austenhsiao@gmail.com>
2020-11-22 23:50:55 -0800

    can delete markers and center

--- pages/index.js
+++ pages/index.js
@@ -32,15 +32,39 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     }
 
+    let counter = 0;
     (sessionStorage.getItem("master").split(',,,,,,,,')).forEach(dataPt => {
         parsedDataPt = JSON.parse(dataPt);
-        addSpot(parsedDataPt.title, parsedDataPt.specie, parsedDataPt.detail, parsedDataPt.location);
+        addSpot(counter, parsedDataPt.title, parsedDataPt.specie, parsedDataPt.detail, parsedDataPt.location);
         addMarker(parsedDataPt.location);
+        ++counter;
     });
 });
 
-function addSpot(name, specie, detail, latlng){
-    document.getElementById("coordinatesBox").innerHTML += `Name: ${name}<br>Species: ${specie}<br>Details: ${detail}<br>Location: ${latlng}<br><br>`
+function addSpot(counter, name, specie, detail, latlng){
+    let div = document.createElement("div");
+    div.style.display = "block";
+    div.innerHTML = `Name: ${name}<br>Species: ${specie}<br>Details: ${detail}<br>Location: ${latlng}`;
+    document.getElementById("coordinatesBox").appendChild(div);
+
+    let buttonCenter = document.createElement("button");
+    buttonCenter.style.display = "block";
+    buttonCenter.style.border = "none";
+    buttonCenter.style.background = "lightskyblue";
+    buttonCenter.innerHTML = "Center";
+    div.appendChild(buttonCenter);
+    buttonCenter.addEventListener("click", () => resetCenter(latlng));
+    
+
+    let buttonDelete = document.createElement("button");
+    buttonDelete.style.display = "inline-block";
+    buttonDelete.style.border = "none";
+    buttonDelete.style.background = "firebrick";
+    buttonDelete.innerHTML = "Delete spot";
+    div.appendChild(buttonDelete);
+    buttonDelete.addEventListener("click", () => removeSpot(counter));
+
+    div.style.border = "solid";
 };
 
 function initMap(){
@@ -48,16 +72,38 @@ function initMap(){
         zoom: 13,
         center: { lat: 45.5111, lng: -122.6834 }, //portland state university
     });
-
-
+    window.markersList = [];
 };
 
 function addMarker(latlngString){
     let parsedLocation = latlngString.split(', ');
     let parsedLat = parseFloat(parsedLocation[0]);
     let parsedLng = parseFloat(parsedLocation[1]);
-    new google.maps.Marker({
+    let marker = new google.maps.Marker({
         map,
         position: {lat: parsedLat, lng: parsedLng},
     });
+    markersList.push(marker);
+    
 };
+
+function resetCenter(latlngString){
+    let parsedLocation = latlngString.split(', ');
+    let parsedLat = parseFloat(parsedLocation[0]);
+    let parsedLng = parseFloat(parsedLocation[1]);
+    map.setCenter({lat: parsedLat, lng: parsedLng});
+};
+
+function removeSpot(num){
+    if(!confirm("Are you sure you want to delete this foraging location?")){
+        return;
+    }
+    markersList[num].setMap(null);
+    var removeDiv = document.querySelector(`#coordinatesBox div:nth-child(${num+1})`);
+    removeDiv.style.display = "none";
+    let master = sessionStorage.getItem("master").split(',,,,,,,,');
+    master.splice(num, 1);
+    sessionStorage.setItem("master", master.join(",,,,,,,,"));
+    sessionStorage.setItem("newSpot", ""); // this prevents data stored in session from reappearing as a forage spot
+    return;
+}
